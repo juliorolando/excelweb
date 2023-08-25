25fd7a501728019217edfeaea99e6685758f4a6a
jrfueguino <juliiitorzj8@gmail.com>
2023-08-25 04:52:38 -0300

    agrego funcion de contabilizar huespedes totales que ingresan - fix bugs menores

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const XLSX = require('xlsx');
 const path = require('path');
-const logic = require('./routes/logic'); 
+const logic = require('./routes/logic');
 
 const app = express();
 const port = 3000; // Puedes cambiar el puerto si es necesario
@@ -17,18 +17,26 @@ const worksheet = workbook.Sheets['Meses'];
 app.use((req, res, next) => {
     req.worksheet = worksheet; // Pasar worksheet como propiedad de la solicitud
     next();
-  });
+});
 
 app.get('/', (req, res) => {
     const maxCellsToShow = 75; // Cambia esto al número de celdas que deseas mostrar
-  const columnToRead = 'ALG';/* tu columna */
-  const palabraEspecial = 'twin';
-
-  const tableRows = logic.obtenerTablaRows(req.worksheet, maxCellsToShow, columnToRead, palabraEspecial);
-  // const tableHTML = tableRows.join('')  
-  res.render('resultados', {tableRows, palabraEspecial: palabraEspecial.toLowerCase()});
+    const columnToRead = 'AZC';/* tu columna */
+    const palabraEspecial = 'twin';
+    const tableRows = logic.obtenerTablaRows(req.worksheet, maxCellsToShow, columnToRead);
+    const huespedesFinal = tableRows[tableRows.length - 1].totalHuespedes
+    res.render('resultados', { tableRows, huespedesFinal, palabraEspecial: palabraEspecial.toLowerCase() })
 });
 
+app.get('/fecha', (req, res) => {
+    console.log(req.query)
+  });
+  
+  app.get('/procesar', (req, res) => {
+    const fecha = req.query.fecha;
+    res.send(`Fecha seleccionada: ${fecha}`);
+  });
+
 app.listen(port, () => {
-  console.log(`Servidor web activo en http://localhost:${port}`);
+    console.log(`Servidor web activo en http://localhost:${port}`);
 });
